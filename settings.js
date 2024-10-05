58c61d109a46318023da9142acb270c2f41427ac
abuhurrraira <141210913+abuhurrraira@users.noreply.github.com>
2024-10-05 05:53:40 +0500

    Update settings.js

--- settings.js
+++ settings.js
@@ -10,14 +10,14 @@ const fs = require('fs')
 const chalk = require('chalk')
 
 //contact details
-global.ownernomer = "923444844060"
-global.ownername = "Qasim Ali"
+global.ownernomer = "923464529577"
+global.ownername = "Ch GOJO"
 global.ytname = "YT: GlobalTechInfo"
 global.socialm = "GitHub: GlobalTechInfo"
 global.location = "Pakistan, Karachi, Karachi"
 
-global.ownernumber = '923444844060'  //creator number
-global.ownername = 'Qasim Ali' //owner name
+global.ownernumber = '923464529577'  //creator number
+global.ownername = 'Ch GOJO' //owner name
 global.botname = 'XeonBug-V8' //name of the bot
 
 //sticker details
